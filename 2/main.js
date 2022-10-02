5baa7491181a784bbc6fbfda719448bb4eb51602
ShivanG <89482670+shiv7700@users.noreply.github.com>
2022-10-02 19:59:04 +0530

    Add files via upload

---
+++ 2/main.js
@@ -0,0 +1,94 @@
+var a;
+a=5;
+// alert(a);
+// statement 
+
+
+// this is a comment 
+
+// single line 
+/* multi 
+line 
+comment */
+
+// rule for naming a variable
+
+var variable=1;
+var variable_1=3;
+alert(variable);
+
+
+var test_1=50;
+var test_2=100;
+
+var final_answer=test_1+test_2;
+console.log(final_answer);
+//learn bodmass rule for arithmetic oeprations 
+
+
+// there are three types of variables in js 
+var type_1=2;
+let type_2=3;
+const type_3=4;
+
+// storing string or text in variable 
+var ggwp="the text";
+let ggwp1='the text also';
+
+console.log(ggwp);
+console.log(ggwp1);
+
+// declare two variable in one statement 
+var text_type="text type" , number_type=1;
+// var text_type1="text" , let number_type2=2; this line will give error 
+
+var de; // declear
+de=1; // assign
+
+var de1=1;
+var de1='string';
+var de1=0.2;
+
+// assignment operator 
+var assignment_1;
+var assignment_2=10;
+assignment_1=assignment_2;
+console.log(assignment_1);
+
+// addition operator 
+let x1=10;
+let x2=55;
+x3=x1+x2;
+console.log(x3);
+
+// multiply operator 
+x3=x1*x2;
+console.log(x3);
+
+// divide operator 
+x3=x2/x1;
+
+// modular operator 
+x3=x2%x1;
+
+// power operator
+x1 = x1** 4;
+
+// adding text to number
+x1 = x1 + x2 + " " + "text added";
+
+// increament and decreament 
+let ince;
+ince=ince+1;
+ince++;
+
+
+
+
+
+
+
+
+
+
+
---
+++ js-1/first.js
@@ -0,0 +1,9 @@
+alert('hello');
+
+document.getElementById("h").innerHTML="change the content";
+
+document.write("this is added by js");
+
+console.log("in the console");
+
+// this is a comment 
