0a6828bf73f992725d9455d0fce4073e4cd8ee7d
ShivanG <89482670+shiv7700@users.noreply.github.com>
2022-12-17 21:39:20 +0530

    Add files via upload

---
+++ 8/main.js
@@ -0,0 +1,109 @@
+// let book1="physics";
+// let book2="maths";
+// let book3="chemistry";
+
+// console.log(book3);
+
+// let array=["physics","maths","chemistry"];
+
+// let array=new Array("physics","maths","chemistry"); 
+
+// console.log(array[2]);
+
+// let array_2=[1 , 3 , 12.2 , "physics","maths","chemistry"];
+// console.log(array_2[2]);
+
+// console.log(array_2);
+
+
+//change array value
+
+// array_2[3]="not phsyics";
+// console.log(array_2[3]);
+
+
+
+// operations on array
+
+// let array_opt=["a","b","c","d","e","f"];
+// console.log(array_opt);
+
+
+// array length 
+// console.log(array_opt.length);
+
+
+//add new to last of array
+// array_opt.push("g");
+// console.log(array_opt);
+
+//add element at 0 
+// array_opt.unshift("0th");
+// console.log(array_opt);
+
+//remove last array element
+// array_opt.pop();
+// console.log(array_opt);
+
+//remove 0th element
+// array_opt.shift();
+// console.log(array_opt);
+
+//remove element at a postion
+// array_opt.splice(2,4)
+// console.log(array_opt);
+
+//empty array
+// array_opt=[];
+// array_opt.length=0;
+// console.log(array_opt);
+
+//find position of element
+// let position= array_opt.indexOf("d")
+// console.log(position);
+
+//check array or not
+// console.log(Array.isArray(array_opt));
+
+//convert variable into array
+// let array_text="This is array";
+// let array_1=array_text.split(' ')
+// console.log(array_1);
+
+//join operation
+// let gun=["smg" , "p90" , "ak47" , "rifle" , "pistol"];
+// let wordarray= gun.join('-');
+// console.log(wordarray);
+
+//join two array
+// let front=["html" , "css" , "js" , "bootstrap"];
+// let back=["nodejs" , "django" , "golang" , "springboot"];
+// let combine=front.concat(back);
+// add three array
+// let db=["mangodb" , "sql" , "etc"];
+// let combine=front.concat(back,db);
+// console.log(combine);
+
+//make array inside aaray (multidimensional array)
+// let normal=["maths","physics","chemistry","biology"];
+// let bookwithpage=[["maths","300"],["physics","400"],["chemistry","500"],["biology","700"]];
+// let fetch= bookwithpage[3][1];
+// console.log(fetch);
+
+//loop in array
+// let array=["cod","csgo","pubg","gow"];
+// let arrayl=array.length;
+
+// for(let i=0; i<arrayl; i++){
+//     console.log(`Element ${i} is ${array[i]} \n`);
+// }
+
+//loop in array with for each
+
+
+
+
+
+
+
+
